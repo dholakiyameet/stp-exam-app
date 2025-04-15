20ce652972e7adcb378c3dfca1d890678cdf7afb
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 <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>
2025-04-15 13:46:39 +0530

    login module done

--- app/Http/Controllers/auth/AuthController.php
+++ app/Http/Controllers/auth/AuthController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+
 
 
 class AuthController extends Controller
@@ -18,19 +20,20 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
-        dd(1);
-        $fields = $request->validate([
+        $credentials = $request->validate([
             'email' => 'required',
             'password' => 'required',
         ]);
 
-        $user = User::where('email', $fields['username'])->first();
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
 
-        if (!$user || !Hash::check($fields['password'], $user->password)) {
-            return response([
-                'message' => 'Wrong credentials'
-            ]);
+            return redirect()->intended('dashboard');
         }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ])->onlyInput('email');
 
     }
 
